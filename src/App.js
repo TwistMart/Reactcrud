054c1a70ddf0250e6d64952b939ee087e8b4a635
TwistMart <martineelucky1996@gmail.com>
2022-08-20 11:56:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [todoList,setTodoList]= useState([]);
+  const[newTask, setNewTask]=useState("");
+  
+  const handleChange= (event) =>{
+    setNewTask(event.target.value);
+  };
+
+  const AddTask=() =>{
+    const newTodoList=[...todoList,newTask];
+    setTodoList(newTodoList);
+
+  };
+
+  const DeleteTask=(taskName) => {
+    const newTodoList=todoList.filter((task) =>{
+    return task !==taskName
+
+      // if(task===taskName){
+      //   return false;
+      // }else {
+      //   return true;
+      // }
+    
+    });
+  
+  // const newTodoList=todoList.filter((task) =>task !==taskName
+  // )
+
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AddTasks">
+
+    <input onChange={handleChange}/>
+
+    <button onClick={AddTask}> Add Task</button>
+    </div> 
+
+    <div className="lists"></div>  
+    {todoList.map((task) => {
+      return <div>
+      <h1>{task}</h1> 
+      <button onClick={() => DeleteTask(task)}> x </button>
+      </div>;
+    }
+    )}
+    
+     
     </div>
   );
 }
